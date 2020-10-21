1b2cee5e3fa0e0e4d3bbccdea66e3711a170c746
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2020-10-21 12:14:40 +0200

    Allow input nodes to do I/O

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
-import { Graph, GraphReader, GraphSpec } from './graph';
+import { Graph, GraphReader, GraphSpec, INPUT, RULE } from './graph';
 
 /*
 PROBLEM DESCRIPTION:
@@ -18,32 +18,35 @@ const PARSE_LST = "PARSE_LST"; // string[]
 const PARSE_DAT = "PARSE_DAT"; // number
 const EVALUATE_LST = "EVALUATE_LST"; // number
 
+const files : {[k : string] : string} = {};
+
 const spec: GraphSpec = {
-    [FILE_CONTENTS]: null,
-    [PARSE_LST]: (g: GraphReader, file: string): string[] => {
+    [FILE_CONTENTS]: {kind: INPUT, fn : (file : string) => {
+        return files[file];
+    }},
+    [PARSE_LST]:  {kind: RULE, fn : (g: GraphReader, file: string): string[] => {
         const contents = g.get_value(FILE_CONTENTS, file) as string;
         console.log(`${PARSE_LST}(${file})`);
         return contents.split(",");
-    },
-    [PARSE_DAT]: (g: GraphReader, file: string): number => {
+    }},
+    [PARSE_DAT]: {kind: RULE, fn: (g: GraphReader, file: string): number => {
         const contents = g.get_value(FILE_CONTENTS, file) as string;
         console.log(`${PARSE_DAT}(${file})`);
         return Number.parseInt(contents.trim());
-    },
-    [EVALUATE_LST]: (g: GraphReader, file: string): number => {
+    }},
+    [EVALUATE_LST]: {kind: RULE, fn: (g: GraphReader, file: string): number => {
         const list = g.get_value(PARSE_LST, file) as string[];
         const numbers = list.map(item => g.get_value(PARSE_DAT, item) as number);
         console.log(`${EVALUATE_LST}(${file})`);
         return numbers.reduce((x, y) => x + y, 0);
     },
-};
-
+}};
 
 const g = new Graph(spec);
 
-g.set_input(FILE_CONTENTS, "main.lst", "foo.dat,bar.dat");
-g.set_input(FILE_CONTENTS, "foo.dat", "13");
-g.set_input(FILE_CONTENTS, "bar.dat", "42");
+files["main.lst"] = "foo.dat,bar.dat";
+files["foo.dat"] = "13";
+files["bar.dat"] = "42";
 console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 // expected output:
 // PARSE_LST(main.lst)
@@ -52,14 +55,14 @@ console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 // EVALUATE_LST(main.lst)
 // => 55
 
-g.set_input(FILE_CONTENTS, "bar.dat", "24");
+files["bar.dat"] = "24";
 console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 // expected output:
 // PARSE_DAT(bar.dat)
 // EVALUATE_LST(main.lst)
 // => 37
 
-g.set_input(FILE_CONTENTS, "bar.dat", "  24  ");
+files["bar.dat"] = "  24  ";
 console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 // expected output:
 // PARSE_DAT(bar.dat)
@@ -68,10 +71,9 @@ console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 // expected output:
 // => 37
-
-g.set_input(FILE_CONTENTS, "main.lst", "foo.dat,baz.dat");
-g.set_input(FILE_CONTENTS, "bar.dat", "17");
-g.set_input(FILE_CONTENTS, "baz.dat", "0");
+files["main.lst"] = "foo.dat,baz.dat";
+files["foo.dat"] = "13";
+files["baz.dat"] = "0";
 console.log(`=> ${g.get_value(EVALUATE_LST, "main.lst")}`);
 // expected output:
 // PARSE_LST(main.lst)
